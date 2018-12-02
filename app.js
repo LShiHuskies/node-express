9da0162943aa7b7c40820b968fdf927f70c3b0f6
github username <github email address>
2018-12-01 19:47:03 -0500

    views and partials

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const path = require('path');
 // const routes = require('./routes');
 const app = express();
 
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
-app.engine(`hbs`, expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.engine(`hbs`, expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
